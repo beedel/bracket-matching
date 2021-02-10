c46539422ad020ed8c498c229a1064e992d9e5ac
Tomas <37914348+begreff@users.noreply.github.com>
2021-02-10 12:04:21 +0000

    Add comments to bracketMatching.py

--- bracketMatching.py
+++ bracketMatching.py
@@ -4,9 +4,13 @@ def bracketMatching(string):
     s = Stack()
     correctlyFormatted = True 
     index = 0
+    
+    # parse the brackets string
     while index < len(string) and correctlyFormatted:
         char = string[index]
         index += 1
+        
+        # check if the character matches any of the brackets
         if char not in "({[" and char not in "]})":
             pass
         else:
